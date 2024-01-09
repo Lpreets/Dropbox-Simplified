c3a5fab9154efc30b048c91a8a13760b782bdbe5
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2024-01-09 13:49:12 +0100

    ""

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dropbox Clone",
-  description: "Dropbox Clone",
+  description: "Dropbox Clone by Lovepreet Zelenkov Singh",
 };
 
 export default function RootLayout({
